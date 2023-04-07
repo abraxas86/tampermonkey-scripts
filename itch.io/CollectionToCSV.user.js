8ac5d193c02eb4f23f06d792f3bf7a3b45e0ce56
abraxas86 <wardrich@gmail.com>
2023-04-07 15:15:36 -0400

    Update CollectionToCSV.user.js
    Removed Javascript require.  Wasn't needed, and was conflicting with and breaking regular site interactions.

--- itch.io/CollectionToCSV.user.js
+++ itch.io/CollectionToCSV.user.js
@@ -5,7 +5,6 @@
 // @description  Scroll down to the bottom of your collection, click the button, get CSV of your collection!
 // @author       Abraxas86
 // @match        https://itch.io/c/*
-// @require      https://code.jquery.com/jquery-3.6.4.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js
 // @grant        none
@@ -37,7 +36,7 @@
         $('.gif_label').remove();
 
         //scrape the game names from the code
-        $('.game_link').each(function(){raw.push($(this).text())})
+        $('.game_link').each(function(){raw.push($(this).text());});
         // Clean up array.  Raw contains some empty strings that we don't need.
         for (var i =0; i < raw.length; i++)
         { if (raw[i] != "") { games.push(raw[i]) } }
